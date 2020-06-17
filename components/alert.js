9f4d41f7c690ca39b0b58fd7bbfd4c39be6c59a9
Jenn Scheer <singleportrait@gmail.com>
2020-06-17 16:18:05 -0600

    Update alert to only show on preview

--- components/alert.js
+++ components/alert.js
@@ -5,14 +5,13 @@ import { EXAMPLE_PATH } from '../lib/constants'
 export default function Alert({ preview }) {
   return (
     <div
-      className={cn('border-b', {
+      className={cn('border-b', 'relative', 'z-10', {
         'bg-accent-7 border-accent-7 text-white': preview,
         'bg-accent-1 border-accent-2': !preview,
       })}
     >
-      <Container>
+      {preview &&
         <div className="py-2 text-center text-sm">
-          {preview ? (
             <>
               This is page is a preview.{' '}
               <a
@@ -23,20 +22,8 @@ export default function Alert({ preview }) {
               </a>{' '}
               to exit preview mode.
             </>
-          ) : (
-            <>
-              The source code for this blog is{' '}
-              <a
-                href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
-                className="underline hover:text-success duration-200 transition-colors"
-              >
-                available on GitHub
-              </a>
-              .
-            </>
-          )}
         </div>
-      </Container>
+      }
     </div>
   )
 }
