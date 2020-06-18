5160ba246d5b8f013855af3ef7eabce9576e78df
Jenn Scheer <singleportrait@gmail.com>
2020-06-18 14:53:00 -0600

    Remove border on alert

--- components/alert.js
+++ components/alert.js
@@ -4,12 +4,7 @@ import { EXAMPLE_PATH } from '../lib/constants'
 
 export default function Alert({ preview }) {
   return (
-    <div
-      className={cn('border-b', 'relative', 'z-10', {
-        'bg-accent-7 border-accent-7 text-white': preview,
-        'bg-accent-1 border-accent-2': !preview,
-      })}
-    >
+    <>
       {preview &&
         <div className="py-2 text-center text-sm">
             <>
@@ -24,6 +19,6 @@ export default function Alert({ preview }) {
             </>
         </div>
       }
-    </div>
+    </>
   )
 }
