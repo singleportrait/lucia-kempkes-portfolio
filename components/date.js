06b9e3c37b93bb9e578f9bea049cd39013c5533b
Jenn Scheer <singleportrait@gmail.com>
2020-06-22 19:34:52 -0600

    Delete unused components

--- components/date.js
+++
@@ -1,9 +0,0 @@
-import { format } from 'date-fns'
-
-export default function Date({ dateString }) {
-  return (
-    <time dateTime={dateString}>
-      {format(new Date(dateString), 'LLLL	d, yyyy')}
-    </time>
-  )
-}
