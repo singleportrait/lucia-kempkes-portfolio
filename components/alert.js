9819aba479f6c07762cb131381a6f9e77b1690cf
Jenn Scheer <singleportrait@gmail.com>
2020-06-22 19:42:02 -0600

    Style preview alert

--- components/alert.js
+++ components/alert.js
@@ -1,21 +1,24 @@
-import Container from './container'
+import styles from './alert.module.css'
 import cn from 'classnames'
 
 export default function Alert({ preview }) {
   return (
     <>
       {preview &&
-        <div className="py-2 text-center text-sm">
-            <>
+        <div className={styles.alert}>
+          <div class="grid">
+            <div class="grid-left" />
+            <div class="grid-center">
               This is page is a preview.{' '}
               <a
                 href="/api/exit-preview"
-                className="underline hover:text-cyan duration-200 transition-colors"
+                className={styles.alertLink}
               >
                 Click here
               </a>{' '}
               to exit preview mode.
-            </>
+            </div>
+          </div>
         </div>
       }
     </>
