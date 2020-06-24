760ca53fc555902609b09a7680e9a28755e27679
Jenn Scheer <singleportrait@gmail.com>
2020-06-23 18:26:32 -0600

    WIP: Use window.innerHeight to calculate proper image height on mobile

--- components/homepageImages.js
+++ components/homepageImages.js
@@ -1,9 +1,20 @@
+import { useState, useEffect } from 'react'
 import styles from './homepageImages.module.scss'
 import cn from 'classnames'
 
 export default function HomepageImages(props) {
+  const [innerHeight, setInnerHeight] = useState("0");
+  useEffect(() => {
+    setInnerHeight(window.innerHeight + "px");
+  })
+
   return (
-    <div className={styles.homepageImagesContainer}>
+    <div
+      className={styles.homepageImagesContainer}
+      style={{
+        height: `calc(${innerHeight} - var(--header-height))`
+      }}
+    >
       <div className={styles.homepageImages}>
         { props.projects.map(({fields: { slug, horizontalPreviewImage, verticalPreviewImage }}, i) =>
           <React.Fragment key={i}>
@@ -13,13 +24,17 @@ export default function HomepageImages(props) {
                   <div
                     className={cn(styles.homepageVerticalImage, props.activeProject === slug && styles.show)}
                     style={{
-                      backgroundImage: `url(${verticalPreviewImage.fields.file.url})`
+                      backgroundImage: `url(${verticalPreviewImage.fields.file.url})`,
+                      height: `calc(${innerHeight} - var(--header-height) - var(--footer-links-height) - 2rem)`,
                     }}
                   />
                 }
                 { verticalPreviewImage?.fields.file.contentType.includes("video") &&
                   <div
                     className={cn(styles.homepageVerticalImage, props.activeProject === slug && styles.show)}
+                    style={{
+                      height: `calc(${innerHeight} - var(--header-height) - var(--footer-links-height) - 2rem)`,
+                    }}
                   >
                     "Move preview background"
                   </div>
