6fa60bae964b7f401e6a88a16be8b2b4897b9dff
Jenn Scheer <singleportrait@gmail.com>
2020-10-20 17:53:51 -0400

    Use Purge CSS whitelist to keep form CSS; final form styling

--- postcss.config.js
+++ postcss.config.js
@@ -14,7 +14,8 @@ module.exports = {
               'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
               'hr',
               'p', 'blockquote', 'ol',
-              '__next'
+              '__next',
+              'input', 'label', 'indicates-required', 'mce_inline_error'
             ],
           },
         ]
